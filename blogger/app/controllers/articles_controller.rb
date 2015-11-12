9cbec6c48a046c37b0c0f1707b9e5d98405c7188
Joseph Berman <josephkberman@outlook.com>
2015-11-12 17:27:14 -0600

    form based workflow feature completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,22 +1,45 @@
 class ArticlesController < ApplicationController
 
+include ArticlesHelper #the security params are stored there
+
+#main article page
 def index
-
 @articles = Article.all
-
 end
 
+#shows you a particular article
 def show
-
 @article = Article.find(params[:id]) 
-
 end
 
-=begin def new
-
-@article = Article.new(params[:id])
-
+#takes you to the page where you WILL create an article
+def new
+@article = Article.new
 end
-=end
+
+#actually creates the article
+def create
+@article = Article.new(article_params)  #passes the article security params from the helper file as an argument
+@article.save
+redirect_to article_path(@article) #sends you to the newly created article page
+end
+
+def destroy
+@article = Article.delete(params[:id])
+redirect_to (articles_path)
+end
+
+def edit
+@article = Article.find(params[:id])
+end
+
+def update
+@article = Article.find(params[:id])
+@article.update(article_params)
+
+flash.notice = "Article '#{@article.title}' Updated!"
+
+redirect_to article_path(@article)
+end
 
 end
