350f8cb351f1177c904f69535b957e87842851e0
Joseph Berman <josephkberman@outlook.com>
2015-11-20 10:10:48 -0600

    added email to the column

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118170241) do
+ActiveRecord::Schema.define(version: 20151120160747) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151118170241) do
     t.integer  "article_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "email"
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
